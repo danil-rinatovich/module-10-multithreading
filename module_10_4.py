2ce7960d9da4981e7bb2ed130627838fd9d8edc7
danil_rinatovich <danil_rinatovich@list.ru>
2024-12-16 03:45:46 +0500

    unfinished

---
+++ module_10_4.py
@@ -0,0 +1,50 @@
+import random
+import threading
+import time
+from queue import Queue
+
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+
+class Guest(threading.Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        rand = random.randint(3, 10)
+        time.sleep(rand)
+
+class Cafe:
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = tables
+
+    def guest_arrival(self, *guests):
+        
+
+    def discuss_guests(self):
+
+
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+# Имена гостей
+guests_names = [
+'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+# Приём гостей
+cafe.guest_arrival(*guests)
+# Обслуживание гостей
+cafe.discuss_guests()
+
+
+
